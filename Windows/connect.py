07cca21638a13b45f196e8d8230e89a6b83726fa
lif28 <160417499+Lif28@users.noreply.github.com>
2026-02-07 20:49:52 +0100

    Add license and header comments to connect.py
    Added licensing information and header comments.

--- Windows/connect.py
+++ Windows/connect.py
@@ -1,3 +1,19 @@
+# PyPass - Password Manager
+# Copyright (C) 2026 Lif28
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from webdav3.client import Client
 from cryptography.fernet import Fernet
 import string
@@ -175,3 +191,4 @@ def Download():
         with open('logs.txt', 'a') as logs:
             logs.write(f'\n[ERR] Download: {e}\n')
         return -2
+
